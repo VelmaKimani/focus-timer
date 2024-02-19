056f494d2a14d9cca39d3d23269308a846d40aa4
calebmutisya <mutisyacalebmusau@gmail.com>
2024-02-19 16:21:12 +0300

    Styling body

--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import './App.css';
 
 export default function Layout() {
   return (
-    <div>
+    <div className='body'>
         <Navbar />
         <Outlet/>
     </div>
