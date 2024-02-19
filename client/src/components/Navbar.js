13cc9ebe31639fd19112ff20cb61adce6dcc9bb8
calebmutisya <mutisyacalebmusau@gmail.com>
2024-02-19 16:39:42 +0300

    Added images folder and imported Images to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './Nav.css';
+import checkring from '../images/Check_ring.svg'
+import reportimg from '../images/Chart_duotone.svg'
+import usercircle from '../images/User_cicrle_light.svg'
+
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+        <div class="logo">
+            <div>
+                <img src={checkring} alt="logo"/>
+            </div>
+            <div class="title">Pomodoro</div>
+        </div>
+        <div class="pages">
+            <div class="navbutton">
+                <img src={reportimg} alt=""/>
+                Report</div>
+            <div class="navbutton">
+                <img src={usercircle} alt=""/>
+                Login</div>
+        </div>
+    </nav>
   )
 }
