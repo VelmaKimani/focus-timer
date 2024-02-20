50c396ced4cfe3aa2c43e599f3baddb6da4549d2
calebmutisya <mutisyacalebmusau@gmail.com>
2024-02-20 15:58:32 +0300

    Home page coded design and styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import '../Home.css'
+import '../Home.css';
+import check from '../images/check.svg'
+import deleteIcon from '../images/Trash.svg'
+import addIcon from '../images/Add.svg'
 
 export default function Home() {
   return (
@@ -18,7 +21,51 @@ export default function Home() {
         <p className='p'># Time to get Busy</p>
       </div>
       <div className='taskscontainer'>
-        Tasks
+        <div className='tasktitle'>Tasks</div>
+        <div className='tasksec'>
+            <img  src={check} alt="check" />
+          <div className='taskdetails'>
+            <div>Shopping</div>
+            <div>Ongoing</div>
+            <div>25 min</div>
+            <div>13/Feb/2024</div>
+            <div>Personal</div>
+            <div>Shop at Naivas</div>
+          </div>
+          <img className='deleteIcon' src={deleteIcon} alt='delete'/>
+        </div>
+        <div className='tasksec'>
+            <img  src={check} alt="check" />
+          <div className='taskdetails'>
+            <div>Shopping</div>
+            <div>Ongoing</div>
+            <div>25 min</div>
+            <div>13/Feb/2024</div>
+            <div>Personal</div>
+            <div>Shop at Naivas</div>
+          </div>
+          <img className='deleteIcon' src={deleteIcon} alt='delete'/>
+        </div>
+        <div className='tasksec'>
+            <img  src={check} alt="check" />
+          <div className='taskdetails'>
+            <div>Shopping</div>
+            <div>Ongoing</div>
+            <div>25 min</div>
+            <div>13/Feb/2024</div>
+            <div>Personal</div>
+            <div>Shop at Naivas</div>
+          </div>
+          <img className='deleteIcon' src={deleteIcon} alt='delete'/>
+        </div>
+        <form className='addtasksec'>
+          <input type='text' placeholder='What are you working on?'/>
+          <input type='number' placeholder='Time in minutes'/>
+          <input type='date' placeholder='date'/>
+          <input type='text' placeholder='Categorize your task'/>
+          <input type='text' placeholder='Add a Note/Description?'/>
+          <button ><img src={addIcon}/>Add Task</button>
+        </form>
       </div>
     </div>
   )
