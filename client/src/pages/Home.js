5e5c55bccc75741c9502d6ae81d707d2ccee36ec
calebmutisya <mutisyacalebmusau@gmail.com>
2024-02-20 13:21:18 +0300

    Timer styled

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import '../Home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div  className="home">
+      <div className='timercontainer'>
+        <div className='breaksect'>
+          <button>Break</button>
+          <button>Continue</button>
+        </div>
+        <div className='timer'>
+          <div className='time'>25:00</div>
+        </div>
+        <div className='start'>
+          START
+        </div>
+        <p className='p'># Time to get Busy</p>
+      </div>
+      <div className='taskscontainer'>
+        Tasks
+      </div>
+    </div>
   )
 }
