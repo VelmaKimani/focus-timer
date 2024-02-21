4f73c47a8d65cf21e9bfb32e40a32c3af2d9fca1
calebmutisya <mutisyacalebmusau@gmail.com>
2024-02-21 19:28:23 +0300

    Login successfull

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -5,6 +5,7 @@ export const UserContext = createContext()
 
 export default function UserProvider({children}) {
 
+  const [authToken, setAuthToken] = useState('');
 
   function Signup(username,email,password){
     fetch('/api/auth/signup', {
@@ -33,7 +34,44 @@ export default function UserProvider({children}) {
       .catch((err) => console.log(err))
   }
 
-  const contextData={Signup}
+  function Login(username, password) {
+    fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else if (res.status === 401) {
+        throw new Error('Invalid credentials');
+      } else {
+        throw new Error('Login failed');
+      }
+    })
+    .then((response) => {
+      sessionStorage.setItem('authToken', response.token);
+      setAuthToken(response.token);
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Login successful.',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: 'error',
+        text: error.message,
+      });
+    });
+
+  }
+
+  const contextData={Signup,Login, authToken, setAuthToken}
 
   return (
     <UserContext.Provider value={contextData}>{children}</UserContext.Provider>  )
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,24 +1,63 @@
-import React from 'react'
+import React, { useContext,useState, useCallback } from 'react'
 import {NavLink} from 'react-router-dom'
 import '../Signup.css'
 import user from '../images/usernameicon.svg'
 import message from '../images/Message.svg'
 import password from '../images/password.svg'
+import { UserContext } from '../context/UserContext'
+
 
 export default function Login() {
+
+  const {Login}= useContext(UserContext)
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const { username, password } = formData;
+      Login(username, password);
+    },
+    [formData, Login]
+  );
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
   return (
-    <form>
-      <p className="title">Login</p>
+    <form onSubmit={handleSubmit}>
+      <p className="title" >Login</p>
         <div className="slotcontainer">
           <img src={user} alt="Username"/>
-          <input type="text" placeholder="Username"/>
+          <input 
+            type="text" 
+            name='username' 
+            placeholder="Username"
+            value={formData.username} 
+            onChange={handleChange}
+          />
         </div>
         <div className="slotcontainer">
             <img src={message} alt="password"/>
-            <input type="password" placeholder="Password"/>
+            <input 
+            type="password" 
+            name='password' 
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            />
         </div>
         <div className="regcontainer">
-            <button onclick="submit()">Login</button>
+            <button type='submit'>Login</button>
         </div>
         <div className="loginsect">
             <p>Don't have an account?</p>
