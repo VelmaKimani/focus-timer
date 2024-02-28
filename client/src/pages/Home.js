580e25cf07bb2f5ce797a2fea161f81ec2176ecc
Velma Kimani <stanvelma16@gmail.com>
2024-02-28 20:05:06 +0300

    Add timer to each task

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,115 +1,128 @@
-import '../Home.css';
+import "../Home.css";
 
-import React, { useEffect, useState, useContext } from 'react'
-import { TasksContext } from '../context/TasksContext';
-import { UserContext } from '../context/UserContext';
+import React, { useContext, useEffect, useState } from "react";
 
-import addIcon from '../images/Add.svg'
-import check from '../images/check.svg'
-import deleteIcon from '../images/Trash.svg'
-import updateIcon from '../images/update.svg'
-import { useNavigate } from 'react-router-dom';
+import { TasksContext } from "../context/TasksContext";
+import { UserContext } from "../context/UserContext";
+import addIcon from "../images/Add.svg";
+import axios from "axios";
+import check from "../images/check.svg";
+import deleteIcon from "../images/Trash.svg";
+import updateIcon from "../images/update.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-
-  const { createTask, tasks, deleteTask, updateTask } = useContext(TasksContext);
-  const { user} = useContext(UserContext);
+  const { createTask, tasks, deleteTask, updateTask } =
+    useContext(TasksContext);
+  const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
-
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(0);
-  const [counting, setCounting] = useState(false);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    let totalSeconds = hours * 3600 + minutes * 60 + seconds;
-    if (counting && totalSeconds > 0) {
-      const intervalId = setInterval(() => {
-        if (totalSeconds > 0) {
-          totalSeconds--;
-          setTimeLeft(totalSeconds);
-          setHours(Math.floor(totalSeconds / 3600));
-          setMinutes(Math.floor((totalSeconds % 3600) / 60));
-          setSeconds(totalSeconds % 60);
-        } else {
-          clearInterval(intervalId);
-          setCounting(false);
-          showNotification();
-        }
+    // Fetch form data from API
+    fetch("http://127.0.0.1:5000/get_tasks")
+      // fetch("http://127.0.0.1:5000/get_tasks")
+      .then((response) => response.json())
+      .then((data) => {
+        // Update form data state with fetched data
+        console.log(data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching form data:", error);
+      });
+  }, []);
+
+  const [taskList, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [timer, setTimer] = useState({ hours: 0, minutes: 0, seconds: 0 });
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+
+  // useEffect(() => {
+  //   const fetchTasks = async () => {
+  //     const response = await axios.get("http://127.0.0.1:5000/gettasks"); // Assuming Flask server is running on the same host
+  //     setTasks(response.data);
+  //   };
+
+  //   fetchTasks();
+  // }, []);
+
+  const handleTaskSelect = (task) => {
+    setSelectedTask(task);
+    setTimer(task);
+    setIsRunning(false);
+    setIsPaused(false); // Reset pause state when a new task is selected
+  };
+
+  useEffect(() => {
+    let interval;
+    if (isRunning && !isPaused) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => {
+          let { hours, minutes, seconds } = prevTimer;
+          const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+          if (totalSeconds === 0) {
+            clearInterval(interval);
+            setIsRunning(false);
+            alert("Time is up!");
+            return { hours: 0, minutes: 0, seconds: 0 };
+          }
+
+          if (seconds === 0) {
+            if (minutes === 0 && hours !== 0) {
+              hours--;
+              minutes = 59;
+            } else if (minutes !== 0) {
+              minutes--;
+            }
+            seconds = 59; // Set seconds to 0 to prevent negative numbers
+          } else {
+            seconds--;
+          }
+          return { hours, minutes, seconds };
+        });
       }, 1000);
-
-      return () => clearInterval(intervalId);
     }
-  }, [counting, hours, minutes, seconds]);
+    return () => clearInterval(interval);
+  }, [isRunning, isPaused]);
 
-  const handleStart = (event) => {
-    event.preventDefault(); 
-    let totalSeconds = hours * 3600 + minutes * 60 + seconds;
-    setTimeLeft(totalSeconds);
-    setCounting(true);
+  const handleStartTimer = () => {
+    if (selectedTask) {
+      setIsRunning(true);
+    }
   };
 
   const handlePause = () => {
-    setCounting(false);
-  };
-
-  const handleContinue = () => {
-    setCounting(true);
-  };
-
-  const handleHoursChange = (e) => {
-    setHours(parseInt(e.target.value));
-  };
-
-  const handleMinutesChange = (e) => {
-    setMinutes(parseInt(e.target.value));
-  };
-
-  const handleSecondsChange = (e) => {
-    setSeconds(parseInt(e.target.value));
-  };
-
-  const formatTime = (time) => {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
-    return `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  };
-  
-  const showNotification = () => {
-    if (Notification.permission === 'granted') {
-      new alert('Countdown Timer', {
-        body: 'The timer has run out!',
-      });
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-          new Notification('Countdown Timer', {
-            body: 'The timer has run out!',
-          });
-        }
-      });
+    if (selectedTask) {
+      setIsPaused(true);
     }
   };
 
+  const handleContinue = () => {
+    if (selectedTask) {
+      setIsPaused(false);
+    }
+  };
+
+  const formatTime = (time) => {
+    return time.toString().padStart(2, "0");
+  };
 
   // The code below is for add task don't Touch Please!!!
-  const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
-  const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
-  const [hrs, setHrs] = useState('');
-  const [mins,setMins]= useState('');
-  const [secnds,setSecnds]= useState('');
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [hrs, setHrs] = useState("");
+  const [mins, setMins] = useState("");
+  const [secnds, setSecnds] = useState("");
 
   const handleTaskUpdate = (taskId) => {
     // Use navigate to navigate to the update page
     navigate(`/update/${taskId}`);
   };
-
-
 
   const handleTaskSubmit = (event) => {
     event.preventDefault();
@@ -128,79 +141,159 @@ export default function Home() {
     // Call createTask function to add task
     createTask(taskData);
     // Clear form fields after submission
-    setTitle('');
-    setCategory('');
-    setDescription('');
-    setDate('');
-    setHrs('');
-    setMins('');
-    setSecnds('');
+    setTitle("");
+    setCategory("");
+    setDescription("");
+    setDate("");
+    setHrs("");
+    setMins("");
+    setSecnds("");
   };
 
-  
   return (
-    <div  className="home">
-      <div className='timercontainer'>
-        <div className='breaksect'>
-          <button onClick={handlePause}>Pause</button>
-          <button onClick={handleContinue}>Continue</button>
-        </div>
-        <div className='timer'>
-          <div className='time'>
-             {formatTime(timeLeft)}
+    <div className="home">
+      <div className="timercontainer">
+        <div className="timer">
+          <div>
+            <div className="time">
+              {formatTime(timer.hours)}:{formatTime(timer.minutes)}:
+              {formatTime(timer.seconds)}
+            </div>
           </div>
         </div>
-        <button className='start' onClick={handleStart}>
+        <button
+          className="start"
+          onClick={handleStartTimer}
+          disabled={!selectedTask || isRunning}
+        >
           START
         </button>
-        <div className='duration'>
-          <label>Hours</label>
-          <label>Minutes</label>
-          <label>Seconds</label>
+        <div className="breaksect">
+          <button onClick={handlePause} disabled={!isRunning || isPaused}>
+            Pause Timer
+          </button>
+          <button onClick={handleContinue} disabled={!isRunning || !isPaused}>
+            Continue Timer
+          </button>
         </div>
-        <div className='timeinput'>
-          <input type='number' placeholder='0' min="0" value={hours} onChange={handleHoursChange}></input>
-          <input type="number" placeholder='0' min="0" max="59" value={minutes} onChange={handleMinutesChange}></input>
-          <input type="number" placeholder='0' min="0" max="60" value={seconds} onChange={handleSecondsChange}></input>
-        </div>
-        <p className='p'># Time to get Busy</p>
+        <p className="p"># Time to get Busy</p>
       </div>
-      <div className='taskscontainer'>
-        <div className='tasktitle'>Tasks</div>
-          {tasks.map(task => (
-            <div className='tasksec' key={task.id}>
-              <img  src={check} alt="check" />
-              <div className='taskdetails'>
-                <div>{task.title}</div>
-                <div>{task.completed ? 'Completed' : 'Ongoing'}</div>
-                <div>{task.hours} hrs {task.minutes} mins {task.seconds} seconds</div>
-                <div>{new Date(task.date).toDateString()}</div>
-                <div>{task.category}</div>
-                <div>{task.description}</div>
+      <div className="taskscontainer">
+        <div className="tasktitle">Tasks</div>
+        {tasks.map((task, index) => (
+          <div
+            key={index}
+            className="tasksec"
+            onClick={() => handleTaskSelect(task)}
+          >
+            {" "}
+            <img src={check} alt="check" />
+            <div className="taskdetails">
+              <div>{task.title}</div>
+              <div>{task.completed ? "Completed" : "Ongoing"}</div>
+              <div>
+                {task.hours} : {task.minutes} : {task.seconds} 
               </div>
-              {/* Implement update task functionality here */}
-              <img
-                className="updateIcon"
-                src={updateIcon}
-                alt="update"
-                onClick={() => handleTaskUpdate(task.id)}
-              />
-              {/* Implement delete task functionality here */}
-              <img className='deleteIcon' src={deleteIcon} alt='delete' onClick={() => deleteTask(task.id)} />
+              <div>{new Date(task.date).toDateString()}</div>
+              <div>{task.category}</div>
+              <div>{task.description}</div>
             </div>
-          ))}
-        
-        <form className='addtasksec' onSubmit={handleTaskSubmit}>
-          <input type='text' placeholder='What are you working on?' value={title} onChange={(e) => setTitle(e.target.value)}/>
-          <input type='number' placeholder='Hours' value={hrs} min={0} onChange={(e) => setHrs(e.target.value)}/>
+            {/* Implement update task functionality here */}
+            <img
+              className="updateIcon"
+              src={updateIcon}
+              alt="update"
+              onClick={() => handleTaskUpdate(task.id)}
+            />
+            {/* Implement delete task functionality here */}
+            <img
+              className="deleteIcon"
+              src={deleteIcon}
+              alt="delete"
+              onClick={() => deleteTask(task.id)}
+            />
+          </div>
+        ))}
+
+        <form className="addtasksec" onSubmit={handleTaskSubmit}>
+          <input
+            type="text"
+            placeholder="What are you working on?"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Categorize your task"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Add a Note/Description?"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+          {/* <input type='number' placeholder='Hours' value={hrs} min={0} onChange={(e) => setHrs(e.target.value)}/>
           <input type='number' placeholder='Minutes' value={mins} min={0} max={59} onChange={(e) => setMins(e.target.value)}/>
-          <input type='number' placeholder='Seconds' value={secnds} min={0} max={60} onChange={(e) => setSecnds(e.target.value)}/>
-          <input type='date' placeholder='date' value={date} onChange={(e) => setDate(e.target.value)}/>
-          <input type='text' placeholder='Categorize your task' value={category} onChange={(e) => setCategory(e.target.value)}/>
-          <input type='text' placeholder='Add a Note/Description?' value={description} onChange={(e) => setDescription(e.target.value)}/>
-          <button type='submit' ><img src={addIcon}/>Add Task</button>
+          <input type='number' placeholder='Seconds' value={secnds} min={0} max={60} onChange={(e) => setSecnds(e.target.value)}/> */}
+
+          <div className="duration">
+            <label>Hours</label>
+            <label>Minutes</label>
+            <label>Seconds</label>
+          </div>
+          <div className="datetime">
+            <div className="timeinput">
+              <input
+                type="date"
+                placeholder="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                required
+              />
+
+              <div className="timelines">
+                <input
+                  type="number"
+                  placeholder="0"
+                  value={hrs}
+                  min={0}
+                  onChange={(e) => setHrs(e.target.value)}
+                  required
+                />
+
+                <input
+                  type="number"
+                  placeholder="0"
+                  value={mins}
+                  min={0}
+                  max={59}
+                  onChange={(e) => setMins(e.target.value)}
+                  required
+                />
+
+                <input
+                  type="number"
+                  placeholder="0"
+                  value={secnds}
+                  min={0}
+                  max={60}
+                  onChange={(e) => setSecnds(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <button type="submit">
+            <img src={addIcon} />
+            Add Task
+          </button>
         </form>
       </div>
     </div>
-  )
+  );
 }
--- client/src/pages/UpdateTask.js
+++ client/src/pages/UpdateTask.js
@@ -1,124 +1,199 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import '../Home.css';
-import updateIcon from '../images/update.svg';
-import { TasksContext } from '../context/TasksContext';
+import "../Home.css";
 
+import React, { useContext, useEffect, useState } from "react";
+
+import { TasksContext } from "../context/TasksContext";
+import updateIcon from "../images/update.svg";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function UpdateTaskForm() {
+  const { taskId } = useParams();
+  const { updateTask } = useContext(TasksContext);
+  const navigate = useNavigate();
 
-    const { taskId } = useParams();
-    const { updateTask } = useContext(TasksContext);
-    const navigate = useNavigate();
+  // State variables to store task data
+  const [title, setTitle] = useState("");
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [completed, setCompleted] = useState(false);
 
-    // State variables to store task data
-    const [title, setTitle] = useState('');
-    const [hours, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
-    const [date, setDate] = useState('');
-    const [category, setCategory] = useState('');
-    const [description, setDescription] = useState('');
-    const [completed, setCompleted]= useState(false)
+  useEffect(() => {
+    // Fetch task data from the API
+    fetch(`/get_task/${taskId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const taskData = data.tasks;
+        setTitle(taskData.title);
+        setHours(parseInt(taskData.hours)); // Convert hours to integer
+        setMinutes(parseInt(taskData.minutes)); // Convert minutes to integer
+        setSeconds(parseInt(taskData.seconds));
+        setDate(taskData.date);
+        setCategory(taskData.category);
+        setDescription(taskData.description);
+        setCompleted(taskData.completed);
+      })
+      .catch((error) => console.error("Error fetching task:", error));
+  }, [taskId]);
 
-    useEffect(() => {
-        // Fetch task data from the API
-        fetch(`/get_task/${taskId}`)
-            .then(response => response.json())
-            .then(data => {
-                const taskData = data.tasks;
-                setTitle(taskData.title);
-                setHours(parseInt(taskData.hours)); // Convert hours to integer
-                setMinutes(parseInt(taskData.minutes)); // Convert minutes to integer
-                setSeconds(parseInt(taskData.seconds));
-                setDate(taskData.date);
-                setCategory(taskData.category);
-                setDescription(taskData.description);
-                setCompleted(taskData.completed);
-            })
-            .catch(error => console.error('Error fetching task:', error));
-    }, [taskId]);
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    // Function to handle form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    // Convert hours, minutes, and seconds to Python datetime objects
+    const time = new Date();
+    time.setHours(hours);
+    time.setMinutes(minutes);
+    time.setSeconds(seconds);
 
-        // Convert hours, minutes, and seconds to Python datetime objects
-        const time = new Date();
-        time.setHours(hours);
-        time.setMinutes(minutes);
-        time.setSeconds(seconds);
+    console.log(typeof date);
 
-        console.log(typeof date);
+    // Format the date object to a string in ISO 8601 format
+    let formattedDate;
+    if (date instanceof Date) {
+      formattedDate = date.toISOString();
+    } else {
+      // Parse the date string into a JavaScript Date object
+      const parsedDate = date ? new Date(date) : null;
+      formattedDate = parsedDate ? parsedDate.toISOString() : "";
+    }
 
-        // Format the date object to a string in ISO 8601 format
-        let formattedDate;
-        if (date instanceof Date) {
-            formattedDate = date.toISOString();
-        } else {
-            // Parse the date string into a JavaScript Date object
-            const parsedDate = date ? new Date(date) : null;
-            formattedDate = parsedDate ? parsedDate.toISOString() : '';
-        }
+    updateTask(taskId, {
+      title,
+      date: formattedDate,
+      hours,
+      minutes,
+      seconds,
+      category,
+      description,
+      completed,
+    });
+    // Navigate to a different route after updating task
+    navigate("/");
+  };
 
-        updateTask(taskId, { title, date: formattedDate, hours, minutes, seconds, category, description, completed });        
-        // Navigate to a different route after updating task
-        navigate('/');
-        
-    };
-
-    // Function to handle input changes
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        // Update state based on input field name
-        switch (name) {
-            case 'title':
-                setTitle(value);
-                break;
-            case 'hours':
-                setHours(parseInt(value));
-                break;
-            case 'minutes':
-                setMinutes(parseInt(value));
-                break;
-            case 'seconds':
-                setSeconds(parseInt(value));
-                break;
-            case 'date':
-                // Parse the date string into a JavaScript Date object
-                const parsedDate = value ? new Date(value) : '';
-                console.log(typeof parsedDate); // Check the type of the parsedDate
-                setDate(parsedDate);
-                break;
-            case 'category':
-                setCategory(value);
-                break;
-            case 'description':
-                setDescription(value);
-                break;
-            case 'completed':
-                setCompleted(value === 'true');
-                break;
-            default:
-                break;
-        }
-    };
+  // Function to handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // Update state based on input field name
+    switch (name) {
+      case "title":
+        setTitle(value);
+        break;
+      case "hours":
+        setHours(parseInt(value));
+        break;
+      case "minutes":
+        setMinutes(parseInt(value));
+        break;
+      case "seconds":
+        setSeconds(parseInt(value));
+        break;
+      case "date":
+        // Parse the date string into a JavaScript Date object
+        const parsedDate = value ? new Date(value) : "";
+        console.log(typeof parsedDate); // Check the type of the parsedDate
+        setDate(parsedDate);
+        break;
+      case "category":
+        setCategory(value);
+        break;
+      case "description":
+        setDescription(value);
+        break;
+      case "completed":
+        setCompleted(value === "true");
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
-    <div  className="home">
-        <h2>Update Task</h2>
-        <form className='addtasksec' onSubmit={handleSubmit}>
-            <input type='text' name='title' placeholder='What are you working on?' value={title} onChange={handleChange}/>
-            <input type='number' name='hours' placeholder='Hours' value={hours} min={0} onChange={handleChange}/>
-            <input type='number' name='minutes' placeholder='Minutes' value={minutes} min={0} max={59} onChange={handleChange}/>
-            <input type='number' name='seconds' placeholder='Seconds' value={seconds} min={0} max={60} onChange={handleChange}/>
-            <input type='date' name='date' placeholder='date' value={date} onChange={handleChange}/>
-            <input type='text' name='category' placeholder='Categorize your task' value={category} onChange={handleChange}/>
-            <input type='text' name='description' placeholder='Add a Note/Description?' value={description} onChange={handleChange}/>
-            <input type='text' name='completed' placeholder='Is task Completed true or false?' value={completed.toString()} onChange={handleChange}/>
-            <button type='submit' ><img src={updateIcon} alt='update'/>Update Task</button>
-        </form>
+    <div className="home">
+      <h2>Update Task</h2>
+      <form className="addtasksec" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="What are you working on?"
+          value={title}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="Categorize your task"
+          value={category}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Add a Note/Description?"
+          value={description}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="completed"
+          placeholder="Is task Completed true or false?"
+          value={completed.toString()}
+          onChange={handleChange}
+        />
+        <div className="duration">
+          <label>Hours</label>
+          <label>Minutes</label>
+          <label>Seconds</label>
+        </div>
+        <div className="datetime">
+          <div className="timeinput">
+            <input
+              type="date"
+              name="date"
+              placeholder="date"
+              value={date}
+              onChange={handleChange}
+            />
+            <div className="timelines">
+              <input
+                type="number"
+                name="hours"
+                placeholder="Hours"
+                value={hours}
+                min={0}
+                onChange={handleChange}
+              />
+              <input
+                type="number"
+                name="minutes"
+                placeholder="Minutes"
+                value={minutes}
+                min={0}
+                max={59}
+                onChange={handleChange}
+              />
+              <input
+                type="number"
+                name="seconds"
+                placeholder="Seconds"
+                value={seconds}
+                min={0}
+                max={60}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </div>
+        <button type="submit">
+          <img src={updateIcon} alt="update"  sizes="20px"/>
+          Update Task
+        </button>
+      </form>
     </div>
-  )
-};
+  );
+}
